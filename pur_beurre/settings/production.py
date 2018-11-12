f99b0a7667657c8e7e9484e1dc30955f36b35cfe
gil-x <gino.ladowitch@gmail.com>
2018-11-12 19:28:01 +0100

    Production settings.

--- pur_beurre/settings/production.py
+++ pur_beurre/settings/production.py
@@ -75,10 +75,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'pur_beurre.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pur_beurre',
+        'USER': 'gil',
+        'PASSWORD': 'secret!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
